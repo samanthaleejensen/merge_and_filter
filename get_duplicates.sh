76a619617ee339d8353152174ca76d392b4ec356
samanthaleejensen <samleejensen@gmail.com>
2020-06-02 10:08:13 -0700

    Reorganizing repository

--- get_duplicates.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-awk '{print $1}' dbsnp144_plink_mapped_variants_with_alleles.txt | uniq -c | while read num dupe; do [[ $num > 1 ]] && grep -- "^${dupe}\t" dbsnp144_plink_mapped_variants_with_alleles.txt; done
